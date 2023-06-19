d62f5e4a3e8ee604b627d5ac37b7c8c2432ceea3
v_hhuima_dr <v_hhuima@tencent.com>
2023-06-19 15:52:06 +0800

    初始化

--- LangChain_study/Test.py
+++ LangChain_study/Test.py
@@ -3,17 +3,17 @@
 
 
 import os
-import openai
+from LangChain_study.common import ChatParam
+
+os.environ["OPENAI_API_KEY"] = ChatParam.OPENAI_API_KEY
+# 使用代理访问openai的接口，对于“v1/chat/completions”的api来说，需要在代理后面加一个v1才能构成一个完整的地址
+os.environ["OPENAI_API_BASE"] = ChatParam.OPENAI_API_BASE
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage
 )
-
-os.environ["OPENAI_API_KEY"] = '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'
-# 使用代理访问openai的接口，对于“v1/chat/completions”的api来说，需要在代理后面加一个v1才能构成一个完整的地址
-os.environ["OPENAI_API_BASE"] = 'https://service-n5tgsaik-1254280433.usw.apigw.tencentcs.com/release/v1'
 
 chat  = ChatOpenAI(model_name="gpt-3.5-turbo"
                    ,max_tokens=2048)
